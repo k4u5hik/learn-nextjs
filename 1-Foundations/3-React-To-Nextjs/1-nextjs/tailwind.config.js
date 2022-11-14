8060b2560c1186c01fb8a637826230f5dd2467ad
Kaushik Chemburkar <attherate@gmail.com>
2022-11-15 08:33:01 +1100

    installed tailwind heroicons and headlessui

---
+++ 1-Foundations/3-React-To-Nextjs/1-nextjs/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,jsx,tsx}", "./components/**/*.{html,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

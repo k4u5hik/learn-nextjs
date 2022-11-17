6760e1f98fa5bab9789ab3acdfcad5a0d059bf53
Kaushik Chemburkar <attherate@gmail.com>
2022-11-17 11:37:38 +1100

    Dynamic Routes implemented

--- 2-CreateNextjsApp/1-FirstApp/nextjs-blog/pages/posts/[id].js
+++ 2-CreateNextjsApp/1-FirstApp/nextjs-blog/pages/posts/[id].js
@@ -1,17 +1,24 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
 
   export async function getStaticPaths() {
       const paths = getAllPostIds();
@@ -21,13 +28,15 @@ export default function Post({ postData }) {
       };
     }
     
-export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+    export async function getStaticProps({ params }) {
+      // Add the "await" keyword like this:
+      const postData = await getPostData(params.id);
+    
+      return {
+        props: {
+          postData,
+        },
+      };
+    }
 
 
